ef78803f4de1f7dfc209b1162a01f3d81548b4ae
Javid Mirzeyev <javid.mirzeyev+demo@gmail.com>
2024-01-02 22:12:18 +0000

    weather

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
 "use strict";
 $(document).ready(function () {
 
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&appid=fe57130549e94791f490784ddc5b1ff8&units=metric`;
+            async function myweather() {
+                let responce = await fetch(url);
+                let data = await responce.json();
+                $("city").html(data.name);
+                $("#temp").html(data.main.temp);
+                $("#feels").html(data.main.feels_like);
+                $("#myweather").val("");
+                console.log(data)
+            }
+            myweather()
+        }
+    });
+
+    $("#btn").click(function (e) {
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&appid=fe57130549e94791f490784ddc5b1ff8&units=metric`;
+        async function myweather() {
+            let responce = await fetch(url);
+            let data = await responce.json();
+            $("city").html(data.name);
+            $("#temp").html(data.main.temp);
+            $("#feels").html(data.main.feels_like);
+            $("#myweather").val("");
+            console.log(data)
+        }
+        myweather()
+
+    });
+
+
+
+
+
+
+
+
+
+
 });
